5d51a3ccf25ff48a9c3930c842607399580d60a5
FREDVUNI <fredvuni809@gmail.com>
2023-02-25 10:31:30 +0300

    truncate title and add css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({title,category,rating,price,linkImg}) => {
     <div className='bg-white drop-shadow-md overflow-hidden mr-2 my-4'>
         <img src={blog1} alt="courses"  className='h-40 w-full object-cover'/>
         <div className='p-5'>
-            <h1 className='py-2'>{title}</h1>
+            <h1 className='py-2 truncate'>{title}</h1>
             <Rating rating={3}/>
         </div>
         <h3 className='p-5 text-xl bottom-0'>{price}</h3>

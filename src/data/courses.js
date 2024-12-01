19428fe43224c72f2ca50fd2223bd81fdc76d42d
Fred Vuni <41730664+FREDVUNI@users.noreply.github.com>
2024-12-01 15:47:55 +0300

    chore: update course image link

--- src/data/courses.js
+++ src/data/courses.js
@@ -14,7 +14,7 @@ export const courses = [
       rating:4,
       price: '$59.99',
       linkImg:
-        'https://imgs.search.brave.com/ZkwGbElTOq8Zo7NeRU669-o016O6XSLJftZZ5I--HJg/rs:fit:750:422:1/g:ce/aHR0cHM6Ly9kaXNj/b3VudHNlcmllcy5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMTEvMTAwLWRh/eXMtb2YtY29kZS10/aGUtY29tcGxldGUt/cHl0aG9uLXByby1i/b290Y2FtcC1mb3It/MjAyMi1jb3Vyc2Uu/anBn',
+        'https://media.graphassets.com/output=format:jpg/resize=height:800,fit:max/OJpmsDheSdGOyi1mgsp6',
     },
     {
       id: 3,
